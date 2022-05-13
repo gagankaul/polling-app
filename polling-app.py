254ca7cdb295bf97d1dc02835d056ca4f7f2c4b3
Gagan Kaul <gagankaul@gmail.com>
2022-05-13 11:46:02 -0400

    refactor and update

--- polling-app.py
+++ polling-app.py
@@ -8,39 +8,36 @@ issue = input("\nWhat is the yes or no issue you will be voting on today: ")
 num_voters = int(input("What is the number of voters you will allow on the issue: "))
 passwd = input("Enter a password for viewing the results of the poll: ")
 
-#Create a dictionary to save polled users and their response
+#Initialize variables
 results = {}
+yes = 0
+no = 0
 
 #The main program loop
 for voter in range(num_voters):
     name = input("\nEnter your full name: ").title()
-    if name not in results:
+    if name not in results.keys():
         print(f"Here is our issue: {issue}")
         response = input("What do you think ... yes or no: ").lower().strip()
-        if (response != "yes") and (response != "no"):
+        if (response != "yes" and response != "y") and (response != "no" and response != "n"):
             print("That's not a valid response")
         else:
-            if response == "yes":
+            if response == "yes" or response == "y":
                 results[name] = "yes"
+                yes += 1
             else:
                 results[name] = "no"
-            print(f"Thank you, {name}! Your vote of yes has been recorded.")
+                no += 1
+            print(f"Thank you, {name}! Your vote of {response} has been recorded.")
     else:
         print("Sorry, someone with this name has already voted.")
 
-#Display names of people who voted
-print(f"\nThe following {len(results)} people voted: ")
+#Display names of voters
+print(f"\nThe following {len(results.keys())} people voted: ")
 for key in results.keys():
     print(key)
 
 #Display the results of the poll
-yes = 0
-no = 0
-for value in results.values():
-    if value == "yes":
-        yes += 1
-    else:
-        no += 1
 print(f"\nOn the following issue: {issue}")
 if yes > no:
     print(f"Yes wins! {yes} votes to {no}.")
@@ -55,5 +52,7 @@ user_passwd = input("\nPlease enter the password to view polling results: ")
 if user_passwd == passwd:
     for key,value in results.items():
         print(f"Voter: {key} \t\t Vote: {value}")
+else:
+    print("Sorry, you seem to have entered an incorrect password. Goodbye....")
 
 print("\nThank you for using the Yes or No Issue Polling App.")
